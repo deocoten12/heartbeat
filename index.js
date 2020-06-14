a53f0aba51218100af6918befc93f21866d7619e
Van Ha <hacoi98x@gmail.com>
2020-06-14 15:41:00 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,257 @@
+var express = require('express')
+var mysql = require('mysql')
+var mqtt = require('mqtt')
+
+var app = express()
+var port = 6060
+
+app.use(express.static("public"))
+app.set("views engine", "ejs")
+app.set("views", "./views")
+
+var server = require("http").Server(app)
+var io = require('socket.io')(server)
+app.get("/", function(req, res){
+    res.render("dothi.ejs");
+});
+app.get("/dothi.ejs", function(req, res){
+    res.render("dothi.ejs");
+});
+app.get("/lichsu.ejs", function(req, res){
+    res.render("lichsu.ejs");
+});
+app.get("/thaynguong.ejs", function(req,  res){
+    res.render("thaynguong.ejs");
+});
+server.listen(port, function(){
+    console.log('Server listening on port ' + port)
+})
+
+//----------------------MQTT-------------------------
+// var client = mqtt.connect("mqtt://localhost:1883",{clientId:"mqttjs01"}); 
+var client = mqtt.connect("mqtt://broker.hivemq.com",{clientId:"mqttjs01"});   
+
+var topic2 = "orion-canhbao"; //LED cảnh báo hoặc còi ...
+var message="test message";
+var topic1 = "orion-heartbeat"
+
+console.log("connected flag  " + client.connected);
+client.on("connect",function(){ 
+    console.log("connected  "+ client.connected);   
+    });
+
+client.on("error",function(error){
+    console.log("Can't connect" + error);
+    process.exit(1)});  
+
+client.subscribe("orion-heartbeat");
+client.subscribe("tat-thu-cong");
+
+//----------------------------------------------------
+
+// SQL--------------------------------------
+// var con = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'datnguyen',
+//     password : '123456',
+//     database : 'sinhvien'
+// });
+
+var con = mysql.createConnection({
+ host     : 'us-cdbr-east-06.cleardb.net',
+ user     : 'b315b8ae360c48',
+ password : 'd07d4384',
+ database : 'heroku_46d626e7af30ec0'
+});
+
+con.connect(function(err) {
+	if (err) 
+		throw err;
+	console.log("mysql connected");
+    var sql ="CREATE TABLE IF NOT EXISTS sensors12(ID int(10) not null, Time datetime not null, Heartbeat int(3) not null, Oxygen int(3) not null )"
+	con.query(sql, function(err){
+		if (err) 
+			throw err;
+        console.log("Table created");
+    });
+    var sql1 = "SELECT * FROM sensors12 ORDER BY ID DESC limit 1"
+        con.query(sql1, function(err, result, fields){
+            if (err) throw err;
+            result.forEach(function(value) {
+                console.log("max id: " + value.ID);
+                count=value.ID
+        })
+    });
+})
+
+
+var oxy_graph=[]; 
+var heartbeat_graph=[]; 
+var date_graph=[];
+
+var m_time;
+var newHeartbeat;
+var newOxy;
+var nguongcao; 
+var nguongthap; 
+var mode=1;
+var count;
+
+
+client.on('message',function(topic, message, packet){
+    console.log("message is "+ message)
+    console.log("topic is "+ topic)
+    
+    newHeartbeat = JSON.parse(message).Heartbeat
+    newOxy = JSON.parse(message).Oxygen
+
+    if(topic == "tat-thu-cong"){
+        io.sockets.emit("tat-thu-cong", "")
+    }
+
+    if(topic == topic1 && newHeartbeat != null && newOxy != null && newHeartbeat>0 && newOxy>0){
+
+        console.log("ready to save")
+        count = count + 1
+        var n = new Date()//.toMysqlFormat();
+        var month = n.getMonth() + 1
+        var Date_and_Time = n.getFullYear()+"-"+month+"-"+n.getDate() +" "+ n.getHours()+":"+n.getMinutes()+":"+n.getSeconds();
+        // console.log(Date_and_Time)
+        var sql = "INSERT INTO sensors12 (ID, Time, Heartbeat, Oxygen) VALUES ('" + count + "', '" + Date_and_Time.toString() + "', '" + newHeartbeat + "', '" + newOxy + "')"
+        con.query(sql, function(err, result){
+            if (err) throw err;
+            console.log("Table inserted");
+            console.log(Date_and_Time +" "+ newHeartbeat +" "+ newOxy)
+                    // })       
+                        // console.log(result)
+                        // console.log(tempData)
+        });
+        var sql1 = "SELECT * FROM sensors12 ORDER BY ID DESC limit 1"
+        con.query(sql1, function(err, result, fields){
+            if (err) throw err;
+            console.log("Data selected");
+            result.forEach(function(value) {
+                m_time = value.Time.toString().slice(4,24);
+                newHeartbeat = value.Heartbeat
+                newOxy = value.Oxygen
+                console.log(m_time + " " + value.Heartbeat + " " + value.Oxygen);
+                // tempFulldata.push({id: value.ID, time: m_time, temp: value.Temperature, humi: value.Humidity})
+                io.sockets.emit('server-update-data', {id: value.ID, time: m_time, beat: value.Heartbeat, oxy: value.Oxygen})
+            })       
+            // console.log(result)
+            // console.log(tempData)
+
+            if (oxy_graph.length < 20) {
+                oxy_graph.push(newOxy);
+            }
+            else{
+                for(i=0;i<19;i++){
+                    oxy_graph[i]=oxy_graph[i+1];
+                }
+                oxy_graph[19]=newOxy;
+            }
+
+            if (heartbeat_graph.length<20) {
+                heartbeat_graph.push(newHeartbeat);
+            }
+            else{
+                for(u=0;u<19;u++){
+                    heartbeat_graph[u]=heartbeat_graph[u+1];
+                }
+                heartbeat_graph[19]=newHeartbeat;
+            }
+
+            if (date_graph.length<20) {
+                date_graph.push( m_time);
+            }
+            else{
+                for(x=0;x<19;x++){
+                    date_graph[x]=date_graph[x+1];
+                }
+                date_graph[19]=m_time;
+            }
+            
+            console.log(heartbeat_graph)
+
+            io.sockets.emit("server-send-oxy_graph",oxy_graph);
+            io.sockets.emit("server-send-heartbeat_graph",heartbeat_graph);
+            io.sockets.emit("server-send-date_graph",date_graph);
+            //  io.sockets.emit("nguongcao",nguongcao);
+            // io.sockets.emit("nguongthap",nguongthap);
+            if(((newHeartbeat>=nguongcao)||(newHeartbeat<=nguongthap))&&(mode==1)){
+                io.sockets.emit("Canhbao");
+                mode = 0;
+                client.publish(topic2, "on")
+                io.sockets.emit("Canhbao-off");
+            }
+            if(((newHeartbeat<nguongcao)&&(newHeartbeat>nguongthap))&&(mode==0)){
+                mode = 1;
+                io.sockets.emit("Canhbao-on");
+                client.publish(topic2, "off")
+            }
+        });
+    }
+       // if(((newHeartbeat>=nguongcao)||(newHeartbeat<=nguongthap))&&(mode==1)){
+       //  io.sockets.emit("Canhbao");
+       //  mode = 0;
+       //  io.sockets.emit("Canhbao-off");
+       //  }
+       //  if(((newHeartbeat<nguongcao)&&(newOxy>nguongthap))&&(mode==0)){
+       //  mode = 1;
+       //  io.sockets.emit("Canhbao-on");
+       //  }
+},5000)
+
+//-----------------------------------------
+//Socket
+
+// io.sockets.emit("send-full", tempData)
+io.on('connection', function(socket){
+    console.log(socket.id + " connected")
+    socket.on('disconnect', function(){
+        console.log(socket.id + " disconnected")
+    })
+     io.sockets.emit("server-send-oxy_graph",oxy_graph);
+    io.sockets.emit("server-send-heartbeat_graph",heartbeat_graph);
+    io.sockets.emit("server-send-date_graph",date_graph);
+
+    var sql1 = "SELECT * FROM sensors12 ORDER BY ID"
+    con.query(sql1, function(err, result, fields){
+        if (err) throw err;
+        console.log("Full Data selected");
+        var tempFulldata = []
+        result.forEach(function(value) {
+            var m_time = value.Time.toString().slice(4,24);
+            tempFulldata.push({id: value.ID, time: m_time, beat: value.Heartbeat, oxy: value.Oxygen})
+        })
+        io.sockets.emit('send-full', tempFulldata)
+    })
+    //io.sockets.emit('send-full', tempFulldata)
+    if(mode==1){
+        io.sockets.emit("Canhbao-on");
+        }else{
+        io.sockets.emit("Canhbao-off");
+    }
+    socket.on("thaynguong1",function(data){
+        nguongcao=data;
+        console.log(data);
+    });
+    socket.on("canhbao",function(data){
+        console.log(data);
+        mode = data;
+    });
+
+    socket.on("thaynguong2",function(data){
+        nguongthap=data;
+        console.log(data);
+        io.sockets.emit("thaynguong-thanhcong");
+    });
+
+    socket.on("tat-thu-cong-web",function(){
+         client.publish(topic2, "off")
+    });
+    });
+   setInterval(function(){ 
+     io.sockets.emit("nguongcao",nguongcao);
+    io.sockets.emit("nguongthap",nguongthap);
+    }, 5000);
---
+++ public/xuly.js
@@ -0,0 +1,271 @@
+var socket = io("http://localhost:6060")
+var page;
+var itemPerPage = 15;
+var id_arr=[];
+var time_arr=[];
+var beat_arr=[];
+var oxy_arr=[];
+var begin_page;
+ var audio = new Audio('canhbao.mp3');
+  audio.loop = true;
+socket.on("send-full", function(data){
+    $("#table-left1").html("")
+    $("#table-center").html("")
+    $("#table-right").html("")
+    $("#table-left2").html("")
+    data.forEach(function(item){
+         id_arr.push(item.id)
+         time_arr.push(item.time)
+         beat_arr.push(item.beat)
+         oxy_arr.push(item.oxy)
+    }) 
+    begin_page=Math.floor(id_arr.length/itemPerPage);
+    page=begin_page;
+    $("#current").html("");
+    $("#current").append("<div class='page'</div>"+(begin_page+1)+"/"+(begin_page+1));
+    for(let i = begin_page*itemPerPage; i < id_arr.length ; i++)
+    {       
+        $("#table-left1").append("<div class='para'>"+time_arr[i]+"</div>")
+        $("#table-center").append("<div class='para'>"+beat_arr[i]+"</div>")
+        $("#table-right").append("<div class='para'>"+oxy_arr[i]+"</div>")
+        $("#table-left2").append("<div class='para'>"+id_arr[i]+"</div>")
+    }
+     if(page==begin_page){
+        $("#page-back").hide();
+        $("#ve-trang-dau").hide();
+     }
+     if(page==begin_page && page ==0){
+        $("#page-back").hide();
+        $("#page-next").hide();
+     }
+});
+socket.on("server-update-data", function(data){
+
+    // $("#current-beat").html("")
+    // $("#current-oxy").html("")
+    // $("#current-beat").append("Nhiệt độ: "+data.beat+"°C")
+    // $("#current-oxy").append("Độ ẩm :"+data.oxy+" %")
+    
+
+  //   var v = data.beat;
+  //   $('.beat-stem-perct').css('height',2*v+'%');
+  // if(v<=10){
+  //    $('.beat-stem-perct').css('background','#A9F5F2');
+  //    $('.bulb').css('background','#A9F5F2');
+  // }
+  // else if(v<=20){
+  //    $('.beat-stem-perct').css('background','#A9F5A9');
+  //    $('.bulb').css('background','#A9F5A9');
+  // }
+  // else if(v<=30){
+  //    $('.beat-stem-perct').css('background','#F3F781');
+  //    $('.bulb').css('background','#F3F781');
+  // }
+  // else if(v<=40){
+  //    $('.beat-stem-perct').css('background','#FE9A2E');
+  //    $('.bulb').css('background','#FE9A2E');
+  // }
+  // else{
+  //   $('.beat-stem-perct').css('background','#FE2E2E');
+  //   $('.bulb').css('background','#FE2E2E');
+  // }
+
+
+  // var u = data.oxy;
+  //   $('.oxy-stem-perct').css('height',u+'%');
+  // if(u<=20){
+  //    $('.oxy-stem-perct').css('background','#00FFFF');
+  // }
+  // else if(u<=40){
+  //    $('.oxy-stem-perct').css('background','#00BFFF');
+  // }
+  // else if(u<=60){
+  //    $('.oxy-stem-perct').css('background','#0080FF');
+  // }
+  // else if(u<=80){
+  //    $('.oxy-stem-perct').css('background','#0040FF');
+  // }
+  // else{
+  //   $('.oxy-stem-perct').css('background','#08088A');
+  // }
+
+
+    id_arr.push(data.id);
+    time_arr.push(data.time);
+    beat_arr.push(data.beat);
+    oxy_arr.push(data.oxy);
+    updateTable();
+    if(id_arr.length==(begin_page+1)*itemPerPage){
+        begin_page=begin_page+1;
+         $("#current").html("");
+        $("#current").append("<div class='page'</div>"+(page+1)+"/"+(begin_page+1));
+        $("#page-back").show();
+        $("#ve-trang-dau").show();
+    }
+
+})
+
+function updateTable() {
+    if(id_arr.length < (begin_page+1)*itemPerPage+1 && page==begin_page){
+        $("#table-left1").append("<div class='para'>"+time_arr[id_arr.length-1]+"</div>")
+        $("#table-center").append("<div class='para'>"+beat_arr[id_arr.length-1]+"</div>")
+        $("#table-right").append("<div class='para'>"+oxy_arr[id_arr.length-1]+"</div>")
+        $("#table-left2").append("<div class='para'>"+id_arr[id_arr.length-1]+"</div>")
+    }
+ } 
+socket.on("thaynguong-thanhcong", function(){
+     $('#thaynguong-status').html("");
+     $('#thaynguong-status').show();
+     $('#thaynguong-status').append("Thay Đổi Ngưỡng Thành Công");
+     setTimeout(function(){ 
+        $('#thaynguong-status').hide();
+    }, 5000);
+   
+ });
+socket.on("Canhbao-on", function(){
+    $('#checked').attr('checked', true);
+    $('#warning-show').hide();
+     audio.pause();
+ });
+socket.on("Canhbao-off", function(){
+     $('#checked').attr('checked', false);
+     $('#warning-show').show();
+ });
+socket.on("Canhbao", function(){
+    $('#warning').show();
+    audio.play();
+ });
+
+socket.on("tat-thu-cong", function(){
+    $("#warning").hide();
+        audio.pause();
+})
+     
+
+$(document).ready(function(){
+   $("#page-back").click(function() {
+        page = page + 1;
+        $("#table-left1").html("");
+        $("#table-center").html("");
+        $("#table-right").html("");
+        $("#table-left2").html("");
+         $("#current").html("");
+    $("#current").append("<div class='page'</div>"+(page+1)+"/"+(begin_page+1));
+        for(let i = page*itemPerPage; i < id_arr.length && i < (page+1)*itemPerPage; i++)
+        {       
+            $("#table-left1").append("<div class='para'>"+time_arr[i]+"</div>")
+            $("#table-center").append("<div class='para'>"+beat_arr[i]+"</div>")
+            $("#table-right").append("<div class='para'>"+oxy_arr[i]+"</div>")
+            $("#table-left2").append("<div class='para'>"+id_arr[i]+"</div>")
+        }
+    if(page==begin_page){
+        $("#page-back").hide();
+        $("#ve-trang-dau").hide();
+        $("#ve-trang-cuoi").show();
+     }
+     else{
+        $("#page-next").show();
+        $("#ve-trang-dau").show();
+        $("#ve-trang-cuoi").show();
+    }    
+    });
+     $("#page-next").click(function() {
+        page = page - 1;
+        $("#table-left1").html("");
+        $("#table-center").html("");
+        $("#table-right").html("");
+        $("#table-left2").html("");
+         $("#current").html("");
+        $("#current").append("<div class='page'</div>"+(page+1)+"/"+(begin_page+1));
+        for(let i = page*itemPerPage; i < id_arr.length && i < (page+1)*itemPerPage; i++)
+        {       
+            $("#table-left1").append("<div class='para'>"+time_arr[i]+"</div>")
+            $("#table-center").append("<div class='para'>"+beat_arr[i]+"</div>")
+            $("#table-right").append("<div class='para'>"+oxy_arr[i]+"</div>")
+            $("#table-left2").append("<div class='para'>"+id_arr[i]+"</div>")
+        }
+        if(page==0){
+        $("#page-next").hide();
+        $("#ve-trang-dau").show();
+        $("#ve-trang-cuoi").hide();
+     }
+     else{
+        $("#page-back").show();
+        $("#ve-trang-dau").show();
+        $("#ve-trang-cuoi").show();
+    }    
+    }); 
+
+
+       $("#ve-trang-dau").click(function() {
+        page = begin_page;
+        $("#table-left1").html("");
+        $("#table-center").html("");
+        $("#table-right").html("");
+        $("#table-left2").html("");
+         $("#current").html("");
+        $("#current").append("<div class='page'</div>"+(page+1)+"/"+(begin_page+1));
+        for(let i = page*itemPerPage; i < id_arr.length && i < (page+1)*itemPerPage; i++)
+        {       
+            $("#table-left1").append("<div class='para'>"+time_arr[i]+"</div>")
+            $("#table-center").append("<div class='para'>"+beat_arr[i]+"</div>")
+            $("#table-right").append("<div class='para'>"+oxy_arr[i]+"</div>")
+            $("#table-left2").append("<div class='para'>"+id_arr[i]+"</div>")
+        }
+        if(page==begin_page){
+        $("#ve-trang-dau").hide();
+        $("#page-back").hide();
+        $("#ve-trang-cuoi").show();
+        $("#page-next").show();
+     }
+    }); 
+
+         $("#ve-trang-cuoi").click(function() {
+        page = 0;
+        $("#table-left1").html("");
+        $("#table-center").html("");
+        $("#table-right").html("");
+        $("#table-left2").html("");
+         $("#current").html("");
+        $("#current").append("<div class='page'</div>"+(page+1)+"/"+(begin_page+1));
+        for(let i = page*itemPerPage; i < id_arr.length && i < (page+1)*itemPerPage; i++)
+        {       
+            $("#table-left1").append("<div class='para'>"+time_arr[i]+"</div>")
+            $("#table-center").append("<div class='para'>"+beat_arr[i]+"</div>")
+            $("#table-right").append("<div class='para'>"+oxy_arr[i]+"</div>")
+            $("#table-left2").append("<div class='para'>"+id_arr[i]+"</div>")
+        }
+        if(page==0){
+        $("#ve-trang-cuoi").hide();
+        $("#page-next").hide();
+        $("#ve-trang-dau").show();
+        $("#page-back").show();
+     }
+    }); 
+
+
+    $('#thaynguong-status').hide();
+    $("#thaynguong-button").click(function(){
+        socket.emit("thaynguong1", $("#pin1").val());
+        socket.emit("thaynguong2", $("#pin2").val());
+    });
+    $("#warning-button").click(function(){
+        $("#warning").hide();
+        audio.pause();
+        socket.emit("tat-thu-cong-web","");
+    });
+    $('#checked').change(function(){
+    if(this.checked) {
+        socket.emit("canhbao",1);
+    }
+    else {
+        socket.emit("canhbao",0);
+    }
+    });
+})
+
+
+// setInterval(function(){
+//     var whatTimeIsThis = new Date()
+//     document.getElementById("RTC").innerHTML = whatTimeIsThis.toString().slice(0,34)
+// }, 1000)
